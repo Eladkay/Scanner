ed994f5bdea7dcd95b2db743d2055523f595cb5e
Elad Kinsbruner <eladkay@gmail.com>
2016-09-07 19:36:41 +0300

    Block by block, model

--- src/main/java/eladkay/scanner/TileEntityScanner.java
+++ src/main/java/eladkay/scanner/TileEntityScanner.java
@@ -1,8 +1,9 @@
 package eladkay.scanner;
 
 import cofh.api.energy.IEnergyReceiver;
+import net.darkhax.tesla.api.BaseTeslaContainer;
+import net.darkhax.tesla.capability.TeslaCapabilities;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
@@ -11,42 +12,77 @@ import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import javax.annotation.Nullable;
 
 public class TileEntityScanner extends TileEntity implements IEnergyReceiver, ITickable {
 
-	public int energy;
     public static final int MAX = 300000;
+    int x = -1;
+    int y = -1;
+    int z = -1;
+
+    public void onBlockActivated() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
 
 	@Override
 	public void update() {
         ChunkProviderServer cps = FMLCommonHandler.instance().getMinecraftServerInstance().worldServerForDimension(0).getChunkProvider();
         Chunk chunk = cps.provideChunk(pos.getX(), pos.getZ());
-        for(int x = 0; x < 16; x++)
+
+        if(x < 0 || y < 0 || z < 0) return;
+        x++;
+        container.takePower(100000, false);
+        markDirty();
+        IBlockState block = chunk.getBlockState(x, y, z);
+        worldObj.setBlockState(new BlockPos(x + pos.getX(), y, z + pos.getZ()), block, 2);
+        if(x >= 16) {
+            y++;
+            x = 0;
+        }
+        if(y >= 80) {
+            z++;
+            y = 0;
+        }
+
+        /*for(int x = 0; x < 16; x++)
             for(int z = 0; z < 16; z++)
-                for(int y = 0; y < 256; y++)
-                    if(worldObj.getBlockState(new BlockPos(x, y, z)).getBlock() == Blocks.AIR)  //i know this can be compacted
-                        if(energy >= 100000) {
-                            energy -= 100000;
-                            IBlockState block = chunk.getBlockState(x, y, z);
-                            worldObj.setBlockState(new BlockPos(x + pos.getX(), y, z + pos.getZ()), block, 2);
-                        }
+                for(int y = 0; y < 256; y++) {*/
+
+               // }
+
+                      //  } // if(container.getStoredPower() >= 100000) {//i know this can be compacted
 
 	}
 
-	@Override
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        NBTTagCompound tag = new NBTTagCompound();
+        writeToNBT(tag);
+        return tag;
+    }
+
+    @Override
 	public void readFromNBT(NBTTagCompound nbt) {
 		super.readFromNBT(nbt);
-		energy = nbt.getInteger("energy");
+        this.container = new BaseTeslaContainer(nbt.getCompoundTag("TeslaContainer"));
+        BlockPos pos = BlockPos.fromLong(nbt.getLong("positions"));
+        x = pos.getX();
+        y = pos.getY();
+        z = pos.getZ();
+
 	}
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
-
 		super.writeToNBT(nbt);
-		nbt.setInteger("energy", energy);
+        nbt.setTag("TeslaContainer", this.container.serializeNBT());
+        nbt.setLong("positions", new BlockPos(x, y, z).toLong());
 		return nbt;
 	}
 
@@ -57,16 +93,17 @@ public class TileEntityScanner extends TileEntity implements IEnergyReceiver, IT
 
 	@Override
 	public int receiveEnergy(EnumFacing from, int maxReceive, boolean simulate) {
-        int energyReceived = Math.min(MAX - energy, maxReceive);
+        int energyReceived = Math.min(MAX - (int)container.getStoredPower(), maxReceive);
         if (!simulate) {
-            energy += energyReceived;
+            container.givePower(energyReceived, simulate);
+            markDirty();
         }
         return energyReceived;
 	}
 
 	@Override
 	public int getEnergyStored(EnumFacing from) {
-		return energy;
+		return (int)container.getStoredPower();
 	}
 
 	@Override
@@ -81,5 +118,25 @@ public class TileEntityScanner extends TileEntity implements IEnergyReceiver, IT
         writeToNBT(nbt);
         SPacketUpdateTileEntity packer = new SPacketUpdateTileEntity(getPos(), 0, nbt);
         return packer;
+    }
+    //Tesla
+    private BaseTeslaContainer container;
+
+    public TileEntityScanner() {
+        this.container = new BaseTeslaContainer(MAX, MAX, MAX);
+    }
+
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getCapability (Capability<T> capability, EnumFacing facing) {
+        return capability == TeslaCapabilities.CAPABILITY_CONSUMER || capability == TeslaCapabilities.CAPABILITY_HOLDER ? (T) this.container : super.getCapability(capability, facing);
+
+    }
+
+    @Override
+    public boolean hasCapability (Capability<?> capability, EnumFacing facing) {
+        return capability == TeslaCapabilities.CAPABILITY_CONSUMER || capability == TeslaCapabilities.CAPABILITY_HOLDER || super.hasCapability(capability, facing);
+
     }
 }
