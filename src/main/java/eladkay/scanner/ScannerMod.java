1f5d6af153262e10664a486cadc194d4e40a596b
Elad Kinsbruner <eladkay@gmail.com>
2017-01-19 16:00:54 +0200

    1.4

--- src/main/java/eladkay/scanner/ScannerMod.java
+++ src/main/java/eladkay/scanner/ScannerMod.java
@@ -57,7 +57,7 @@ public class ScannerMod {
     public static ScannerMod instance;
     public static CreativeTabs tab;
     public static BlockAirey air;
-    public static final String VERSION = "1.3.3";
+    public static final String VERSION = "1.4";
 
     @Mod.EventHandler
     public void init(FMLPreInitializationEvent event) {
