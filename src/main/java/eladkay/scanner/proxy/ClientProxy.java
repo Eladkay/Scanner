f788be10f326f8410c9a171fa3a3463ad5e59cb4
Elad Kinsbruner <eladkay@gmail.com>
2017-03-24 13:20:19 +0300

    Closes #17, #18 and a lot of complaints.

--- src/main/java/eladkay/scanner/proxy/ClientProxy.java
+++ src/main/java/eladkay/scanner/proxy/ClientProxy.java
@@ -13,15 +13,8 @@ import net.minecraft.item.Item;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import javax.annotation.Nullable;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Date;
 
 public class ClientProxy extends CommonProxy {
     private static final String IP = "http://eladkay.pw/scanner/ScannerCallback.php";
@@ -49,50 +42,7 @@ public class ClientProxy extends CommonProxy {
 
     }
 
-    /**
-     * There's definitely a better way to do this, but who am I to judge
-     *
-     * @param event The event
-     */
-    @SubscribeEvent
-    public void onClientTick(TickEvent.ClientTickEvent event) {
-        if (sentCallback || Minecraft.getMinecraft().player == null) return;
-        if (Minecraft.getMinecraft().isSnooperEnabled()) {
-            sentCallback = true;
-            System.out.println("Player opted out of Scanner statistics");
-        }
-        try {
-            if (Minecraft.getMinecraft().player.getName().matches("(?:Player\\d{1,3})")) return;
-            new Thread(() -> {
-                try {
-                    sendGet("ScannerMod", IP + "?username=" + Minecraft.getMinecraft().player.getName() + "&timestamp=" + new Date().toString().replace(" ", "") + "&version=" + ScannerMod.VERSION);
-                } catch (Exception e) {
-                    sentCallback = true;
-                    e.printStackTrace();
-                }
-            }).start();
-            //System.out.println(IP + "?username=" + Minecraft.getMinecraft().player.getName() + "&timestamp=" + new Date().toString().replace(" ", "") + "&version=" + ScannerMod.VERSION);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        sentCallback = true;
-    }
 
-    private static String sendGet(String userAgent, String url) throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", userAgent);
-        int responseCode = con.getResponseCode();
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        return response.toString();
-    }
 
     @Override
     public void openGuiBiomeScanner(TileEntityBiomeScanner tileEntity) {
