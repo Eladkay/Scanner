4a08e274c68926dbd575c5268c480c052ad17f05
Elad Kinsbruner <eladkay@gmail.com>
2017-08-05 14:30:54 +0300

    Start work

--- src/main/java/eladkay/scanner/compat/Waila.java
+++ src/main/java/eladkay/scanner/compat/Waila.java
@@ -4,14 +4,13 @@ import com.teamwizardry.librarianlib.common.network.PacketHandler;
 import eladkay.scanner.biome.BlockBiomeScanner;
 import eladkay.scanner.misc.BaseTE;
 import eladkay.scanner.misc.MessageUpdateEnergyServer;
+import eladkay.scanner.terrain.BlockDimensionalRift;
 import eladkay.scanner.terrain.BlockTerrainScanner;
-import mcp.mobius.waila.api.IWailaConfigHandler;
-import mcp.mobius.waila.api.IWailaDataAccessor;
-import mcp.mobius.waila.api.IWailaDataProvider;
-import mcp.mobius.waila.api.IWailaRegistrar;
+import mcp.mobius.waila.api.*;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagLong;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -25,6 +24,9 @@ public class Waila {
 
         registrar.registerBodyProvider(new Scanner(), BlockBiomeScanner.class);
         registrar.registerNBTProvider(new Scanner(), BlockBiomeScanner.class);
+
+        registrar.registerBodyProvider(new Rift(), BlockDimensionalRift.class);
+        registrar.registerNBTProvider(new Rift(), BlockDimensionalRift.class);
     }
 
     public static class Scanner implements IWailaDataProvider {
@@ -66,6 +68,88 @@ public class Waila {
             return tag;
         }
 
+    }
+
+    public static class Rift implements IWailaDataProvider {
+        /**
+         * Callback used to override the default Waila lookup system.</br>
+         * Will be used if the implementing class is registered via {@link IWailaRegistrar}.{@link registerStackProvider}.</br>
+         *
+         * @param accessor Contains most of the relevant information about the current environment.
+         * @param config   Current configuration of Waila.
+         * @return null if override is not required, an ItemStack otherwise.
+         */
+        @Override
+        public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+            return accessor.getStack();
+        }
+
+        /**
+         * Callback used to add lines to one of the three sections of the tooltip (Head, Body, Tail).</br>
+         * Will be used if the implementing class is registered via {@link IWailaRegistrar}.{@link registerHeadProvider} client side.</br>
+         * You are supposed to always return the modified input currenttip.</br>
+         *
+         * @param itemStack  Current block scanned, in ItemStack form.
+         * @param currenttip Current list of tooltip lines (might have been processed by other providers and might be processed by other providers).
+         * @param accessor   Contains most of the relevant information about the current environment.
+         * @param config     Current configuration of Waila.
+         * @return Modified input currenttip
+         */
+        @Override
+        public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+            return currenttip;
+        }
+
+        /**
+         * Callback used to add lines to one of the three sections of the tooltip (Head, Body, Tail).</br>
+         * Will be used if the implementing class is registered via {@link IWailaRegistrar}.{@link registerBodyProvider} client side.</br>
+         * You are supposed to always return the modified input currenttip.</br>
+         *
+         * @param itemStack  Current block scanned, in ItemStack form.
+         * @param currenttip Current list of tooltip lines (might have been processed by other providers and might be processed by other providers).
+         * @param accessor   Contains most of the relevant information about the current environment.
+         * @param config     Current configuration of Waila.
+         * @return Modified input currenttip
+         */
+        @Override
+        public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+            currenttip.add("Ticks until conversion: " + (BlockDimensionalRift.TileDimensionalRift.TICKS_TO_COMPLETION - accessor.getNBTData().getInteger("ticks")));
+            return currenttip;
+        }
+
+        /**
+         * Callback used to add lines to one of the three sections of the tooltip (Head, Body, Tail).</br>
+         * Will be used if the implementing class is registered via {@link IWailaRegistrar}.{@link registerTailProvider} client side.</br>
+         * You are supposed to always return the modified input currenttip.</br>
+         *
+         * @param itemStack  Current block scanned, in ItemStack form.
+         * @param currenttip Current list of tooltip lines (might have been processed by other providers and might be processed by other providers).
+         * @param accessor   Contains most of the relevant information about the current environment.
+         * @param config     Current configuration of Waila.
+         * @return Modified input currenttip
+         */
+        @Override
+        public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+            return currenttip;
+        }
+
+        /**
+         * Callback used server side to return a custom synchronization NBTTagCompound.</br>
+         * Will be used if the implementing class is registered via {@link IWailaRegistrar}.{@link registerNBTProvider} server and client side.</br>
+         * You are supposed to always return the modified input NBTTagCompound tag.</br>
+         *
+         * @param player The player requesting data synchronization (The owner of the current connection).
+         * @param te     The TileEntity targeted for synchronization.
+         * @param tag    Current synchronization tag (might have been processed by other providers and might be processed by other providers).
+         * @param world  TileEntity's World.
+         * @param pos    Position of the TileEntity.
+         * @return Modified input NBTTagCompound tag.
+         */
+        @Override
+        public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
+            tag.setInteger("ticks", ((BlockDimensionalRift.TileDimensionalRift) te).ticks);
+            return tag;
+        }
     }
 }
 
---
+++ src/main/java/eladkay/scanner/terrain/BlockDimensionalRift.java
@@ -0,0 +1,104 @@
+package eladkay.scanner.terrain;
+
+import com.teamwizardry.librarianlib.common.base.block.BlockModContainer;
+import com.teamwizardry.librarianlib.common.base.block.ItemModBlock;
+import com.teamwizardry.librarianlib.common.util.autoregister.TileRegister;
+import com.teamwizardry.librarianlib.common.util.saving.Save;
+import eladkay.scanner.ScannerMod;
+import eladkay.scanner.misc.BaseTE;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.ITickable;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+import static eladkay.scanner.ScannerMod.tab;
+
+public class BlockDimensionalRift extends BlockModContainer {
+
+    @Nullable
+    @Override
+    public ItemBlock createItemForm() {
+        return new ItemModBlock(this) {
+            @Override
+            public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+                if (stack.getItemDamage() == EnumDimensions.NONE.ordinal()) tooltip.add("Crafting component.");
+                else {
+                    tooltip.add("Allows you to create Dimensional Core blocks.");
+                    tooltip.add("Place in the End, Nether, or Overworld, to get the respective");
+                    tooltip.add("dimensional core.");
+                }
+                super.addInformation(stack, playerIn, tooltip, advanced);
+            }
+        };
+    }
+
+    public BlockDimensionalRift() {
+        super("dimensionalRift", Material.IRON);
+        setCreativeTab(tab);
+        setHardness(Blocks.IRON_BLOCK.getBlockHardness(null, null, null));
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(@NotNull World world, @NotNull IBlockState iBlockState) {
+        return new TileDimensionalRift();
+    }
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state) {
+        return EnumBlockRenderType.INVISIBLE;
+    }
+
+    @TileRegister("rift")
+    public static class TileDimensionalRift extends BaseTE implements ITickable {
+
+        public static final int TICKS_TO_COMPLETION = 20 * 60 * 10;
+        public static final int CAPACITY = 1_000_000;
+
+        public TileDimensionalRift() {
+            super(CAPACITY);
+        }
+
+        public @Save
+        int ticks = 0;
+
+        /**
+         * Like the old updateEntity(), except more generic.
+         */
+        @Override
+        public void update() {
+            ticks++;
+            if (ticks >= TICKS_TO_COMPLETION) {
+                transform();
+            }
+        }
+
+        private void transform() {
+            BlockDimensionalCore dimensionalCore = ScannerMod.dimensionalCore;
+            PropertyEnum<EnumDimensions> type = BlockDimensionalCore.TYPE;
+            switch (getWorld().provider.getDimensionType()) {
+                case NETHER:
+                    getWorld().setBlockState(getPos(), dimensionalCore.getDefaultState().withProperty(type, EnumDimensions.NETHER));
+                    break;
+                case THE_END:
+                    getWorld().setBlockState(getPos(), dimensionalCore.getDefaultState().withProperty(type, EnumDimensions.END));
+                    break;
+                case OVERWORLD:
+                default:
+                    getWorld().setBlockState(getPos(), dimensionalCore.getDefaultState().withProperty(type, EnumDimensions.OVERWORLD));
+                    break;
+            }
+        }
+    }
+}
